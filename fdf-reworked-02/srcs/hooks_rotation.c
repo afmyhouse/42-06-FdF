0303fb402460c41767bafbf31382aa08673c11c0
Antonio Ferreira <af.myhouse@gmail.com>
2023-10-28 20:32:12 +0100

    HP 20231028 001

--- fdf-reworked-02/srcs/hooks_rotation.c
+++ fdf-reworked-02/srcs/hooks_rotation.c
@@ -6,7 +6,7 @@
 /*   By: antoda-s <antoda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 16:20:54 by myoung            #+#    #+#             */
-/*   Updated: 2023/10/28 20:27:08 by antoda-s         ###   ########.fr       */
+/*   Updated: 2023/10/28 20:31:29 by antoda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	rotate_hook(t_view *v)
 	ft_printf(" (v->keys->d - v->keys->a) = %d\n", (v->keys->d - v->keys->a));
 	ft_printf(" (v->keys->q - v->keys->e) = %d\n", (v->keys->q - v->keys->e));
 	ft_printf("----------------------\n");
-	v->theta += (v->keys->w - v->keys->s) * 0.05;
-	v->phi += (v->keys->d - v->keys->a) * 0.05;
-	v->psi += (v->keys->q - v->keys->e) * 0.05;
+	v->theta += (v->keys->w - v->keys->s) * v->alfa_i;
+	v->phi += (v->keys->d - v->keys->a) * v->alfa_i;
+	v->psi += (v->keys->q - v->keys->e) * v->alfa_i;
 	ft_printf(" v->theta = %f\n", v->theta);
 	ft_printf(" v->phi = %f\n", v->phi);
 	ft_printf(" v->psi = %f\n", v->psi);
